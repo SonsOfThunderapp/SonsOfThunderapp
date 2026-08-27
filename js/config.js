12961d48e608f08978c2d94bc9b3e7b2a321749c
SonsOfThunderapp <obietv@gmail.com>
2026-08-27 04:03:20 -0400

    20260827-month14: room seat stays; write password on THIS MONTH

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.TB_CONFIG = {
   // Bumped with each production zip so phones can detect a new build
-  APP_BUILD: '20260827-month13',
+  APP_BUILD: '20260827-month14',
   RSVP_PRESENCE: {
     publicOnHome: true,
     seedFloor: 0,
