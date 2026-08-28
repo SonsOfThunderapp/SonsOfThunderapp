ae093737aacdce60454cdc97681711ffad1215fa
SonsOfThunderapp <obietv@gmail.com>
2026-08-28 18:06:23 -0400

    20260828-home-visible pulse config APP_BUILD

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.TB_CONFIG = {
   // Bumped with each production zip so phones can detect a new build
-  APP_BUILD: '20260828-glass-up',
+  APP_BUILD: '20260828-home-visible',
   RSVP_PRESENCE: {
     publicOnHome: true,
     seedFloor: 0,
