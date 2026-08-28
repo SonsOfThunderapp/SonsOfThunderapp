96b90370caaf66296c13c1dae6a1dcb40db32461
SonsOfThunderapp <obietv@gmail.com>
2026-08-28 06:44:01 -0400

    20260828-wake: pulse config APP_BUILD

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.TB_CONFIG = {
   // Bumped with each production zip so phones can detect a new build
-  APP_BUILD: '20260828-chair-mem',
+  APP_BUILD: '20260828-wake',
   RSVP_PRESENCE: {
     publicOnHome: true,
     seedFloor: 0,
