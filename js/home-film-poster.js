1b7c33c71223dd7b5d6f3501619922e2eb421dd1
SonsOfThunderapp <obietv@gmail.com>
2026-08-29 07:35:59 -0400

    20260829-film-art bake glow still as data URI in home-film-poster.js

--- js/home-film-poster.js
+++ js/home-film-poster.js
@@ -1,8 +1,1 @@
-(function () {
-  if (window.__tbHomeFilmPoster) return;
-  window.__tbHomeFilmPoster = true;
-
-  var SRC = 'https://mnsempcgomukcpofgvlm.supabase.co/storage/v1/object/public/thunder-theater/theater/current.mp4';
-  var POSTER = 'data:image/jpeg;base64,PLACEHOLDER_WILL_FAIL';
-  var BOLT = '/assets/icon-official.png';
-})();
+LOAD_FROM_FILE:/tmp/mcp-create-js.json
